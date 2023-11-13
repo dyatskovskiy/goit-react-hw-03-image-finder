07d5592dbc1ffe8b7c5390e229003ee1f3d8bce3
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-11-13 21:39:12 +0200

    finish modal

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Btn = styled.button`
   padding: 8px 16px;
   margin-left: calc(50vw - 90px);
-  /* margin-right: 50vw; */
   border-radius: 2px;
   background-color: #3f51b5;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,29 +1,11 @@
 import { Component } from 'react';
-// import ReactModal from 'react-modal';
 
 import { Modal } from 'components/Modal/Modal';
 import { ImageItem, Image } from './ImageGalleryItem.styled';
 
-// const customStyles = {
-//   content: {
-//     objectFit: 'contain',
-//     overflow: 'hidden',
-//     maxHeight: '90vh',
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//   },
-// };
-
-// ReactModal.setAppElement('#root');
-
 export class ImageGalleryItem extends Component {
   state = {
     isModalOpen: false,
-    a: 0,
   };
 
   openModal = () => {
@@ -31,7 +13,7 @@ export class ImageGalleryItem extends Component {
   };
 
   closeModal = () => {
-    this.setState({ isModalOpen: false, a: 2 });
+    this.setState({ isModalOpen: false });
   };
 
   render() {
@@ -39,12 +21,18 @@ export class ImageGalleryItem extends Component {
     const { webformatURL, largeImageURL } = this.props;
 
     return (
-      <ImageItem onClick={this.openModal}>
-        <Image src={webformatURL} alt="This is result of your search" />
+      <>
+        <ImageItem onClick={this.openModal}>
+          <Image src={webformatURL} alt="This is result of your search" />
+        </ImageItem>
         {isModalOpen && (
-          <Modal imageUrl={largeImageURL} closeModal={this.closeModal} />
+          <Modal
+            imageUrl={largeImageURL}
+            isModalOpen={this.state.isModalOpen}
+            closeModal={this.closeModal}
+          />
         )}
-      </ImageItem>
+      </>
     );
   }
 }
